fd7be89e07597f6969f0d39d5af3a7dacbf23018
wenmingbfx <wenming@buyidefx.com>
2013-02-20 10:59:09 -0500

    to new project

--- gitFirst/src/com/wm/gitfirst/GitTest.java
+++ gitFirst/src/com/wm/gitfirst/GitTest.java
@@ -2,6 +2,6 @@ package com.wm.gitfirst;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Git is cool");
+		System.out.println("Git is cool in new project");
 	}
 }
